28417997cc6b4aa7467f0d81f12600ed095c85f5
Pedro Mossi <pedroluizmossi@gmail.com>
2023-08-20 04:10:56 -0300

    "Add FaceSwap toggle component"
    - The FaceSwap page template has been updated to replace the ion-header element with a custom toggle component called "FaceSwap".
    - The FaceSwap page TypeScript file has been updated to import the ToggleComponent from the menu directory and include it in the imports list.
    - This change improves the user interface by providing a more user-friendly element to toggle the face swapping feature.

--- src/app/face-swap/face-swap.page.ts
+++ src/app/face-swap/face-swap.page.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { ToggleComponent } from '../menu/toggle/toggle.component';
 @Component({
   selector: 'app-face-swap',
   templateUrl: './face-swap.page.html',
   styleUrls: ['./face-swap.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ToggleComponent]
 })
 export class FaceSwapPage implements OnInit {
 
